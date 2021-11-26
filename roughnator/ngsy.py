5672f57366fca4704f00660f55724b792ae531ed
Andrea Falconi <andrea.falconi@gmail.com>
2021-11-26 12:36:23 +0100

    implement convenience to_json method to exclude entity attrs set to none from serialisation.

--- roughnator/ngsy.py
+++ roughnator/ngsy.py
@@ -30,7 +30,7 @@ id='urn:ngsi-ld:Machine:1' type='Machine' AcelR=None fz=None Diam=None ae=None H
 
 4. Don't serialise unset NGSI attributes.
 
->>> machine1.json(exclude_none=True)
+>>> machine1.to_json()
 {"id": "urn:ngsi-ld:Machine:1", "type": "Machine"}
 
 
@@ -43,14 +43,14 @@ AcelR=1.0 fz=2.0 Diam=4.0 ae=5.0 HB=6.0 geom=10.0 Ra=11.0
 
 6. Transform sensors data to machine entity and serialise it.
 
->>> rr.to_machine_entity(entity_id=machine1.id).json(exclude_none=True)
+>>> rr.to_machine_entity(entity_id=machine1.id).to_json()
 {"id": "urn:ngsi-ld:Machine:1", "type": "Machine", "AcelR": {"type": "Number", "value": 1.0}, "fz": {"type": "Number", "value": 2.0}, "Diam": {"type": "Number", "value": 4.0}, "ae": {"type": "Number", "value": 5.0}, "HB": {"type": "Number", "value": 6.0}, "geom": {"type": "Number", "value": 10.0}, "Ra": {"type": "Number", "value": 11.0}}
 
 
 7. Same as (6) but now some readings are missing.
 
 >>> rr = RawReading(AcelR=1, Ra=11)
->>> rr.to_machine_entity(entity_id=machine1.id).json(exclude_none=True))
+>>> rr.to_machine_entity(entity_id=machine1.id).to_json())
 {"id": "urn:ngsi-ld:Machine:1", "type": "Machine", "AcelR": {"type": "Number", "value": 1.0}, "Ra": {"type": "Number", "value": 11.0}}
 
 
@@ -113,6 +113,9 @@ class BaseEntity(BaseModel):
         own_id = f"{self.type}:{unique_suffix}"
         self.id = ld_urn(own_id)
         return self
+
+    def to_json(self) -> str:
+        return self.json(exclude_none=True)
 
     @classmethod
     def from_raw(cls, raw_entity: dict) -> Optional['BaseEntity']:
@@ -179,17 +182,17 @@ class RawReading(BaseModel):
 #
 # machine1 = MachineEntity(id='').set_id_with_type_prefix('1')
 # print(machine1)
-# print(machine1.json(exclude_none=True))
+# print(machine1.to_json())
 #
 # sensors_data = {"AcelR": 1, "fz": 2, "Diam": 4, "ae": 5, "HB": 6, "geom": 10,
 #                 "Ra": 11}
 # rr = RawReading(**sensors_data)
 # print(rr)
-# print(rr.to_machine_entity(entity_id=machine1.id).json(exclude_none=True))
+# print(rr.to_machine_entity(entity_id=machine1.id).to_json())
 #
 # rr = RawReading(AcelR=1, Ra=11)
 # print(rr)
-# print(rr.to_machine_entity(entity_id=machine1.id).json(exclude_none=True))
+# print(rr.to_machine_entity(entity_id=machine1.id).to_json())
 #
 # ai = RoughnessEstimateEntity(
 #     id=machine1.id,
--- tests/unit/test_ngsy.py
+++ tests/unit/test_ngsy.py
@@ -5,3 +5,25 @@ def test_float_attr_serialisation():
     want = '{"type": "Number", "value": 2.3}'
     got = FloatAttr.from_value(2.3).json()
     assert want == got
+
+
+def test_readings_to_machine_entity_json():
+    sensors_data = {"AcelR": 1, "fz": 2, "Diam": 4, "ae": 5, "HB": 6,
+                    "geom": 10, "Ra": 11}
+    rr = RawReading(**sensors_data)
+
+    machine1 = MachineEntity(id='').set_id_with_type_prefix('1')
+    got = rr.to_machine_entity(entity_id=machine1.id).to_json()
+
+    want = '{"id": "urn:ngsi-ld:Machine:1", "type": "Machine", ' \
+           '"AcelR": {"type": "Number", "value": 1.0}, ' \
+           '"fz": {"type": "Number", "value": 2.0}, ' \
+           '"Diam": {"type": "Number", "value": 4.0}, ' \
+           '"ae": {"type": "Number", "value": 5.0}, ' \
+           '"HB": {"type": "Number", "value": 6.0}, ' \
+           '"geom": {"type": "Number", "value": 10.0}, ' \
+           '"Ra": {"type": "Number", "value": 11.0}}'
+    assert want == got
+
+
+test_readings_to_machine_entity_json()
